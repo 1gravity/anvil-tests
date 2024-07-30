cf734d7f3a8d83a69ea44b625106defa4d3482d4
Emanuel Moecklin <emanuel.moecklin@vivint.com>
2024-07-30 12:18:00 +0200

    create example for Provider<T> that doesn't compile with Anvil but does so with Dagger

--- library/src/main/java/com/example/library/BaseClass.kt
+++ library/src/main/java/com/example/library/BaseClass.kt
@@ -1,8 +1,14 @@
 package com.example.library
 
 import javax.inject.Inject
+import javax.inject.Provider
 
 open class BaseClass<T : Any> {
+    // this doesn't compile
+    @Inject
+    lateinit var provider: Provider<T>
+
+    // this compiles
     @Inject
     lateinit var value: T
 }
--- library/src/main/java/com/example/library/SuperClass.kt
+++ library/src/main/java/com/example/library/SuperClass.kt
@@ -1,8 +1,11 @@
 package com.example.library
 
 import javax.inject.Inject
+import javax.inject.Provider
 
 class SuperClass @Inject constructor(): BaseClass<Feature>() {
+    @Inject
+    lateinit var featureProvider: Provider<Feature>
     @Inject
     lateinit var feature: Feature
 }
